aadd5440ca7544790a0c636507c1f89e15330685
KRSNA007 <mr.krishna5582gmail.com>
2026-04-19 23:54:53 +0530

    INITIAL FOLDER SETUP DONE FOR I-Motive assignment

---
+++ src/test/java/ASSIGNMENT/INTERNSHIP/InternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package ASSIGNMENT.INTERNSHIP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
